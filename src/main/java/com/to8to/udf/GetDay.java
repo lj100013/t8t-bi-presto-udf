01af84404784f20512781c2923360c6c6de1ae54
bryanli.li <bryanli.li@corp.to8to.com>
2021-01-05 10:10:30 +0800

    presto-udf

---
+++ src/main/java/com/to8to/udf/GetDay.java
@@ -0,0 +1,117 @@
+package com.to8to.udf;
+
+import io.airlift.slice.Slice;
+import io.airlift.slice.Slices;
+import io.prestosql.spi.function.Description;
+import io.prestosql.spi.function.ScalarFunction;
+import io.prestosql.spi.function.SqlNullable;
+import io.prestosql.spi.function.SqlType;
+import io.prestosql.spi.type.StandardTypes;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public final class GetDay {
+    @ScalarFunction("getday")
+    @SqlType(StandardTypes.VARCHAR)
+    @Description("Returns the day from timestamp")
+    public static Slice timestampToDay(@SqlNullable @SqlType(StandardTypes.BIGINT) Long time) {
+
+        String output = "";
+
+        try {
+            if (time == null || time.toString().length() < 10) {
+                return Slices.utf8Slice(output);
+            } else if (time.toString().length() == 8 && time >= 19700101 && time <= 21000101) {
+                output = time.toString().substring(0, 4) + "-" + time.toString().substring(4, 6) + "-" + time.toString().substring(6, 8);
+                return Slices.utf8Slice(output);
+            }
+            SimpleDateFormat day = new SimpleDateFormat("yyyyMMdd");
+            String timestamp = time.toString();
+            output = day.format(timestamp.length() == 10 ? new Date(Long.parseLong(timestamp) * 1000) : new Date(Long.parseLong(timestamp)));
+        } catch (Exception e) {
+        }
+        return Slices.utf8Slice(output);
+    }
+
+
+    @ScalarFunction("getday")
+    @SqlType(StandardTypes.VARCHAR)
+    @Description("Returns the day from timestamp")
+    public static Slice timestampToDay(@SqlNullable @SqlType(StandardTypes.VARCHAR) Slice times) {
+        String output = "";
+        try {
+            String time = times.toStringUtf8();
+            SimpleDateFormat day = new SimpleDateFormat("yyyyMMdd");
+            if (time == null || time.length() < 10) {
+                return Slices.utf8Slice(output);
+            } else if (time.contains("-")) {
+                try {
+                    time = String.valueOf(day.parse(time).getTime());
+                } catch (Exception e) {
+                    return Slices.utf8Slice(output);
+                }
+            } else if (time.length() == 8 && Long.parseLong(time) >= 19700101 && Long.parseLong(time) <= 21000101) {
+                output = time.substring(0, 4) + "-" + time.substring(4, 6) + "-" + time.substring(6, 8);
+                return Slices.utf8Slice(output);
+            }
+            output = day.format(time.length() == 10 ? new Date(Long.parseLong(time) * 1000) : new Date(Long.parseLong(time)));
+        } catch (Exception e) {
+
+        }
+
+        return Slices.utf8Slice(output);
+    }
+
+    @ScalarFunction("getday")
+    @SqlType(StandardTypes.VARCHAR)
+    @Description("Returns the day from timestamp")
+    public static Slice timestampToDay(@SqlNullable @SqlType(StandardTypes.VARCHAR) Slice times, @SqlNullable @SqlType(StandardTypes.VARCHAR) Slice standard) {
+        String output = "";
+        try {
+            String time = times.toStringUtf8();
+
+            SimpleDateFormat day = new SimpleDateFormat(standard.toStringUtf8());
+            if (time == null || time.length() < 10) {
+                return Slices.utf8Slice(output);
+            } else if (time.contains("-")) {
+                try {
+                    time = String.valueOf(day.parse(time).getTime());
+                } catch (Exception e) {
+                    return Slices.utf8Slice(output);
+                }
+            } else if (time.length() == 8 && Long.parseLong(time) >= 19700101 && Long.parseLong(time) <= 21000101) {
+                output = time.substring(0, 4) + "-" + time.substring(4, 6) + "-" + time.substring(6, 8);
+                return Slices.utf8Slice(output);
+            }
+            output = day.format(time.length() == 10 ? new Date(Long.parseLong(time) * 1000) : new Date(Long.parseLong(time)));
+        } catch (Exception e) {
+        }
+
+        return Slices.utf8Slice(output);
+    }
+
+    @ScalarFunction("getday")
+    @SqlType(StandardTypes.VARCHAR)
+    @Description("Returns the day from timestamp")
+    public static Slice timestampToDay(@SqlNullable @SqlType(StandardTypes.BIGINT) Long time, @SqlNullable @SqlType(StandardTypes.VARCHAR) Slice standard) {
+        String output = "";
+        try {
+            if (time == null || time.toString().length() < 10) {
+                return Slices.utf8Slice(output);
+            } else if (time.toString().length() == 8 && time >= 19700101 && time <= 21000101) {
+                output = time.toString().substring(0, 4) + "-" + time.toString().substring(4, 6) + "-" + time.toString().substring(6, 8);
+                return Slices.utf8Slice(output);
+            }
+            SimpleDateFormat day = new SimpleDateFormat(standard.toStringUtf8());
+            String timestamp = time.toString();
+            output = day.format(timestamp.length() == 10 ? new Date(Long.parseLong(timestamp) * 1000) : new Date(Long.parseLong(timestamp)));
+        } catch (Exception e) {
+        }
+
+        return Slices.utf8Slice(output);
+    }
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/com/to8to/udf/UdfPlugin.java
@@ -0,0 +1,15 @@
+package com.to8to.udf;
+
+import com.google.common.collect.ImmutableSet;
+import io.prestosql.spi.Plugin;
+
+import java.util.Set;
+
+public class UdfPlugin implements Plugin {
+    @Override
+    public Set<Class<?>> getFunctions() {
+        return ImmutableSet.<Class<?>>builder()
+                .add(GetDay.class)
+                .build();
+    }
+}
